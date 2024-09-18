c545207734909e45732bfe368596792dc7e3981d
izzyjay21 <israeldc101@gmail.com>
2024-09-18 09:03:02 +0100

    time complexity

--- main.py
+++ main.py
@@ -13,3 +13,10 @@ def factoral(n):
   else:
     return n * factoral(n-1)
 print(factoral(10))
+
+
+# factorial(5) factor 5 * 24 >120
+#factorial(4) factor 4 * 6 >24
+#factorial(3) factor 3 * 2 >6
+#factorial(2) factor 2 *1 >2
+#factorial(1) factor 1 *1 >1
